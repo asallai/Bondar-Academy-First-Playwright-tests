3cd8d640d375359c5c94e31f645c60fa4c67f6d4
Aniko Sallai <aniko.sallai@gmail.com>
2024-11-26 23:38:24 +0100

    Radio buttons + Formal modifications

--- tests/autowaiting.spec.ts
+++ tests/autowaiting.spec.ts
@@ -1,35 +1,35 @@
 import { test, expect } from '@playwright/test'
 
-test.beforeEach( async ({ page }) => {
-    await page.goto('http://uitestingplayground.com/ajax')
-    await page.getByText('Button Triggering AJAX request').click()
+test.beforeEach(async ({ page }) => {
+	await page.goto('http://uitestingplayground.com/ajax')
+	await page.getByText('Button Triggering AJAX request').click()
 })
 
 test('Auto waiting', async ({ page }) => {
-    const successButton = page.locator('.bg-success')
-    await successButton.click()
+	const successButton = page.locator('.bg-success')
+	await successButton.click()
 
-   // const text = successButton.textContent()
-   // await successButton.waitFor({ state: "attached" })
-   // const text = successButton.allTextContents()
+	// const text = successButton.textContent()
+	// await successButton.waitFor({ state: "attached" })
+	// const text = successButton.allTextContents()
 
-   // expect(text).toContain('Data loaded with AJAX get request.')
+	// expect(text).toContain('Data loaded with AJAX get request.')
 
-    await expect(successButton).toHaveText('Data loaded with AJAX get request.', { timeout: 20000 })    
+	await expect(successButton).toHaveText('Data loaded with AJAX get request.', { timeout: 20000 })
 })
 
 test('Alternative waits', async ({ page }) => {
-    const successButton = page.locator('.bg-success')
-    
-    // wait for element
-    await page.waitForSelector('.bg-success')
+	const successButton = page.locator('.bg-success')
 
-    // wait for particular response
-    await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+	// wait for element
+	await page.waitForSelector('.bg-success')
 
-    // wait for network calls to be completed (NOT RECOMMENDED)
-    await page.waitForLoadState('networkidle')
+	// wait for particular response
+	await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
 
-    const text = await successButton.allTextContents()
-    expect(text).toContain('Data loaded with AJAX get request.')    
+	// wait for network calls to be completed (NOT RECOMMENDED)
+	await page.waitForLoadState('networkidle')
+
+	const text = await successButton.allTextContents()
+	expect(text).toContain('Data loaded with AJAX get request.')
 })
--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,27 +1,53 @@
 import { test, expect } from '@playwright/test'
+import { using } from 'rxjs'
 
-test.beforeEach( async ({ page }) => {
-    await page.goto('http://localhost:4200/')
+test.beforeEach(async ({ page }) => {
+	await page.goto('http://localhost:4200/')
 })
 
 test.describe('Form Layouts page', async () => {
-    test.beforeEach( async ({ page }) => {
-        await page.getByText('Forms').click()
-        await page.getByText('Form Layouts').click()
-    })
+	test.beforeEach(async ({ page }) => {
+		await page.getByText('Forms').click()
+		await page.getByText('Form Layouts').click()
+	})
 
-    test('Input fields', async ({ page }) => {
-        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
-    
-        await usingTheGridEmailInput.fill('test@test.com')
-        await usingTheGridEmailInput.clear()
-        await usingTheGridEmailInput.pressSequentially('test2@test.com', {delay: 50})
+	test('Input fields', async ({ page }) => {
+		const usingTheGridEmailInput = page.locator('nb-card', { hasText: 'Using the Grid' }).getByRole('textbox', { name: 'Email' })
 
-        // Generic assertion
-        const inputValue = await usingTheGridEmailInput.inputValue()
-        expect(inputValue).toEqual('test2@test.com')
+		await usingTheGridEmailInput.fill('test@test.com')
+		await usingTheGridEmailInput.clear()
+		await usingTheGridEmailInput.pressSequentially('test2@test.com', { delay: 50 })
 
-        // Locator assertion
-        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
-    })
+		// Generic assertion
+		const inputValue = await usingTheGridEmailInput.inputValue()
+		expect(inputValue).toEqual('test2@test.com')
+
+		// Locator assertion
+		await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+	})
+
+	test('Radio buttons', async ({ page }) => {
+		const usingTheGridEmailForm = page.locator('nb-card', { hasText: 'Using the Grid' })
+
+		// Get by Label
+		// (Radio button has 'visuall-hidden' class --> 'force: true' is necessary in order to check!)
+		await usingTheGridEmailForm.getByLabel('Option 1').check({ force: true })
+		// Get by Role
+		await usingTheGridEmailForm.getByRole('radio', { name: 'Option 1' }).check({ force: true })
+
+		// Validate the status
+		// Using Generic assertion
+		const radioStatus = usingTheGridEmailForm.getByLabel('Option 1').isChecked()
+		expect(radioStatus).toBeTruthy()
+		// Using Locator assertion
+		await expect(usingTheGridEmailForm.getByLabel('Option 1')).toBeChecked()
+
+		await usingTheGridEmailForm.getByLabel('Option 2').check({ force: true })
+		// Validation by using Generic assertion
+		expect(await usingTheGridEmailForm.getByLabel('Option 1').isChecked()).toBeFalsy()
+		expect(await usingTheGridEmailForm.getByLabel('Option 2').isChecked()).toBeTruthy()
+		// Validation by using Locator assertion
+		await expect(usingTheGridEmailForm.getByLabel('Option 1')).not.toBeChecked()
+		await expect(usingTheGridEmailForm.getByLabel('Option 2')).toBeChecked()
+	})
 })
