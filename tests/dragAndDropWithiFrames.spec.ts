3eaa84ec9a7f2ea0dd0b9a26e10cac437d475dfc
Aniko Sallai <aniko.sallai@gmail.com>
2025-01-18 22:08:22 +0100

    Drag and drop

--- tests/dragAndDropWithiFrames.spec.ts
+++ tests/dragAndDropWithiFrames.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('Drag and Drop with iFrames', async ({ page }) => {
 	await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
 
-	const frame = page.locator('[rel-title="Photo Manager"] iframe')
+	const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
 	await frame.locator('li', { hasText: 'High Tatras 2' }).dragTo(frame.locator('#trash'))
 
 	// More precise control
