bd857cf0f110d25bbe715ee2887acefe7d5e4f8b
Aniko Sallai <aniko.sallai@gmail.com>
2025-01-18 22:05:31 +0100

    Drag and drop

---
+++ tests/dragAndDropWithiFrames.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+
+test('Drag and Drop with iFrames', async ({ page }) => {
+	await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
+
+	const frame = page.locator('[rel-title="Photo Manager"] iframe')
+	await frame.locator('li', { hasText: 'High Tatras 2' }).dragTo(frame.locator('#trash'))
+
+	// More precise control
+	await frame.locator('li', { hasText: 'High Tatras 4' }).hover() // hover over the element
+	await page.mouse.down() // click the mouse above the element
+	await frame.locator('#trash').hover() // move the mouse into the direction where we want to drop our element
+	await page.mouse.up() // release the mouse
+
+	await expect(frame.locator('#trash li h5')).toHaveText(['High Tatras 2', 'High Tatras 4'])
+})
